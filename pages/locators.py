f041b087bd5448f03a907002c1461c400c270d00
korzia <Qwer2345>
2022-09-22 17:37:41 +0300

    add product_page test, page object and locators

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,11 @@ class LoginPageLocators():
     REGISTER_PASSWORD = (By.XPATH, '//input[@name="registration-password1"]')
     REGISTER_CONFIRM_PASSWORD = (By.XPATH, '//input[@name="registration-password2"]')
     REGISTER_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    PRODUCT_PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]/p[1]')
+    CART_PRICE = (By.XPATH, '//div[@class="alertinner "]/p/strong')
+    PRODUCT_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]/h1')
+    PRODUCT_IN_CART_NAME = (By.XPATH, '//div[@id="messages"]/div[1]/div/strong')
+    ADDED_TO_CART_MESSAGE = (By.XPATH, '//div[@id="messages"]/div[1]')
---
+++ pages/product_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON).click()
+
+    ''' seems to be excess and pointless
+    def should_be_added_to_cart_message(self):
+        assert self.is_element_present(*ProductPageLocators.ADDED_TO_CART_MESSAGE), 'No add to cart message found'
+    '''
+
+    def should_be_same_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE).text
+        assert product_price == cart_price, f'Expected {product_price=} to be equal to {cart_price=}'
+
+    def should_be_same_product_names(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        cart_name = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_CART_NAME).text
+        assert product_name == cart_name, f'Expected {product_name=} to be equal to {cart_name=}'
+
