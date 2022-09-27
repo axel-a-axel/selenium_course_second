67b2f33289401a030348726bcb8db2516c4009f8
korzia <Qwer2345>
2022-09-27 12:13:55 +0300

    add parametrization to check different promo

--- test_product_page.py
+++ test_product_page.py
@@ -4,10 +4,11 @@ import pytest
 import time
 from .pages.product_page import ProductPage
 
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
