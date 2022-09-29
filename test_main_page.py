7912edda03233c1e286dcf0fb03f5b8e5f6812ef
korzia <Qwer2345>
2022-09-29 17:24:51 +0300

    add class in the test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -5,20 +5,24 @@ from .pages.basket_page import BasketPage
 
 urls = {'homepage' : ['http://selenium1py.pythonanywhere.com/']}
 
-@pytest.mark.parametrize('link', urls['homepage'])
-def test_guest_should_see_login_link(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+
+    @pytest.mark.parametrize('link', urls['homepage'])
+    def test_guest_should_see_login_link(self,browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
-@pytest.mark.parametrize('link', urls['homepage'])
-def test_guest_can_go_to_login_page(browser, link):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    @pytest.mark.parametrize('link', urls['homepage'])
+    def test_guest_can_go_to_login_page(self, browser, link):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
 @pytest.mark.basketpage
